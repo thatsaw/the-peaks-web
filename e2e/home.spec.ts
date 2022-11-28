029187a60bfbd55a2ac87c4ef836693370deb868
thatsaw <wsaw1776@gmail.com>
2022-11-29 02:26:16 +0630

    Add playwright Add Playwright for E2E testings.

---
+++ e2e/home.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Home page", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/the peaks/i);
+  });
+});
