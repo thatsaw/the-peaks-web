08a3ff83c9a6b9bdbfe5a7e73535daabf11027f4
thatsaw <wsaw1776@gmail.com>
2022-11-29 14:17:16 +0630

    Add github action Add github action for testings.

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,13 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
+      "**/e2e/**",
+    ],
   },
 });
